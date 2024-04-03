42f6464eba1d16a7efd3b53be979e35ed7d237bd
perses <tiago.ventura12@gmail.com>
2024-04-03 18:00:04 +0100

    should be okay for now

--- scanner.py
+++ scanner.py
@@ -1,23 +1,44 @@
-import requests
+import requests, warnings
+
+warnings.filterwarnings("ignore", category=requests.packages.urllib3.exceptions.InsecureRequestWarning)
+
+securityHeaders = ["Content-Security-Policy",
+                   "Permissions-Policy",
+                   "Strict-Transport-Security",
+                   "X-Content-Type-Options"]
 
 def populateHeaders():
     domainsFile = open("domains.txt", "r")
+    domainHeaderDict = {}
     for domain in domainsFile:
-        domainClean = f"http://{domain.strip()}/"
-        print(f"Using domain: {domainClean}")
+        domainClean = f"https://{domain.strip()}/"
         try:
-            result = requests.head(domainClean, allow_redirects=True, timeout=2)
-            print(result.headers)
+            result = requests.head(domainClean, allow_redirects=True, timeout=2, verify=False)
+            domainHeaderDict[domainClean] = dict(result.headers)
         except requests.exceptions.Timeout:
-            print("Timeout")
             continue
         except requests.exceptions.ConnectionError:
-            print("Connection error")
             continue
+    return domainHeaderDict
+
+def checkHeaders(domainHeaderDict):
+    result = {}
+    for header in securityHeaders:
+        lowerCaseHeader = header.lower()
+        result[header] = []
+        for domain in domainHeaderDict:
+            lowerDomainHeaderDict = {key.lower(): value for key, value in domainHeaderDict[domain].items()}
+            if lowerCaseHeader not in lowerDomainHeaderDict:
+                result[header].append(domain)
+    return result
 
 
 def main():
-    populateHeaders()
+    domainHeaderDict = populateHeaders()
+    result = checkHeaders(domainHeaderDict)
+    for header, domains in result.items():
+        print(header)
+        print(domains)
 
 if __name__ == "__main__":
     main()
